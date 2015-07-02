135a01e053ca2a20aa409dc6e9fc3b0e852bab90
Hemanth S.R <hemanthfeb25@gmail.com>
2015-07-02 11:22:23 +0530

    Sudhakar, hemanth | build script for ci.

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-echo "Removing pacs integration from m2 repository."
-rm -rf /Users/hemanths/.m2/repository/org/bahmni/module/pacsintegration
-rm -rf /Users/hemanths/.m2/repository/org.ict4h
-echo "Removed pacs integration from m2 repository."
-
-echo "Building PACS"
-mvn clean install -PIT
-echo "Done."
---
+++ scripts/ci/build.sh
@@ -0,0 +1,8 @@
+echo "Removing pacs integration from m2 repository."
+rm -rf ~/.m2/repository/org/bahmni/module/pacsintegration
+rm -rf ~/.m2/repository/org.ict4h
+echo "Removed pacs integration from m2 repository."
+
+echo "Building PACS"
+/home/jss/apache-maven-3.0.5/bin/mvn clean install -PIT
+echo "Done."
