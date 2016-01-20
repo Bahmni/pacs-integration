7b59fc3e3c932fabe6da2a2565e50168e7755c25
SwathiVarkala <swathivarkala@gmail.com>
2016-01-20 11:59:31 +0530

    Swathi, Rahul | Changing Port as the test failed because 6001 port was used by X server

--- pacs-integration-webapp/src/test/resources/insertModalities.sql
+++ pacs-integration-webapp/src/test/resources/insertModalities.sql
@@ -1,2 +1,2 @@
 -- modality stub server uses the same port and timeout inserted here
-INSERT INTO modality (id, name, description, ip, port, timeout) values (1, 'X-Ray', 'X ray machine', '127.0.0.1', 6001, 2000);
+INSERT INTO modality (id, name, description, ip, port, timeout) values (1, 'X-Ray', 'X ray machine', '127.0.0.1', 1911, 2000);
