572556a04e0167d230aa5d2ce22d2b7a5c1c38f1
tanyaa-tw <tanya.aggarwal@thoughtworks.com>
2025-07-07 11:58:30 +0530

    Tanya|Heena clean commented code

--- pacs-integration-webapp/src/main/java/org/bahmni/module/PacsIntegration.java
+++ pacs-integration-webapp/src/main/java/org/bahmni/module/PacsIntegration.java
@@ -1,21 +1,16 @@
 package org.bahmni.module;
 
 import org.bahmni.module.pacsintegration.repository.CronJobRepository;
-import org.hibernate.SessionFactory;
-import org.hibernate.ejb.HibernateEntityManagerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-//@ComponentScan(basePackages = "org.bahmni.module.*")
 @EnableTransactionManagement
 public class PacsIntegration extends SpringBootServletInitializer {
 
@@ -30,9 +25,4 @@ public class PacsIntegration extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PacsIntegration.class, args);
     }
-
-//    @Bean
-//    public SessionFactory sessionFactory(HibernateEntityManagerFactory hemf) {
-//        return hemf.getSessionFactory();
-//    }
 }
