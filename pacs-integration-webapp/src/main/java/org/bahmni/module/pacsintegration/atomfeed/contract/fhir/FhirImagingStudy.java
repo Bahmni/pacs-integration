c2ec007418f09e67bc51dfa3cc7a1eba7261a667
skjameela276 <jameela.shaik@thoughtworks.com>
2026-01-12 15:15:44 +0530

    AB#4361 | Update . add JsonIgnoreProperties annotation at the fhireReference and FhirImagingStudy

--- pacs-integration-webapp/src/main/java/org/bahmni/module/pacsintegration/atomfeed/contract/fhir/FhirImagingStudy.java
+++ pacs-integration-webapp/src/main/java/org/bahmni/module/pacsintegration/atomfeed/contract/fhir/FhirImagingStudy.java
@@ -1,10 +1,12 @@
 package org.bahmni.module.pacsintegration.atomfeed.contract.fhir;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FhirImagingStudy {
     
     private String resourceType = "ImagingStudy";
--- pacs-integration-webapp/src/main/java/org/bahmni/module/pacsintegration/atomfeed/contract/fhir/FhirReference.java
+++ pacs-integration-webapp/src/main/java/org/bahmni/module/pacsintegration/atomfeed/contract/fhir/FhirReference.java
@@ -1,8 +1,10 @@
 package org.bahmni.module.pacsintegration.atomfeed.contract.fhir;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FhirReference {
     
     private String reference;
