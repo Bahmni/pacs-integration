ecad6da38b401a4a5f8208ebed14bfea19f2fc2d
ravinder kabli <ravinder.kabli@thoughtworks.com>
2026-03-04 17:48:10 +0530

    BAH-4460|Updated Repo License to MPL v2 with HD

---
+++ NOTICE.md
@@ -0,0 +1,17 @@
+# NOTICE
+
+## Pacs Integration
+
+Copyright © 2026 OpenMRS Inc
+
+### Contributors
+
+This repository has got source code generated at Thoughtworks.
+
+### Third-Party Components
+
+This repository includes third-party source code that is licensed separately:
+
+- Maven Wrapper (`mvnw`, `mvnw.cmd`, `.mvn/wrapper/*`)
+  Licensed under the Apache License, Version 2.0.
+  Copyright © the original authors.
--- pacs-integration-webapp/src/test/resources/insertModalities.sql
+++ pacs-integration-webapp/src/test/resources/insertModalities.sql
@@ -1,2 +1,9 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public License,
+-- v. 2.0. If a copy of the MPL was not distributed with this file, You can
+-- obtain one at https://www.bahmni.org/license/mplv2hd.
+--
+-- Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+-- graphic logo is a trademark of OpenMRS Inc.
+
 -- modality stub server uses the same port and timeout inserted here
 INSERT INTO modality (id, name, description, ip, port, timeout) values (1, 'X-Ray', 'X ray machine', '127.0.0.1', 1911, 2000);
